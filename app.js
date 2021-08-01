aba18ce39dbc120751a7fd53a0818db02c7a0976
AyushPun <ayushpun504@gmail.com>
2021-08-01 16:24:48 +0530

    commit 3

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var a = document.querySelector("#add");
 var s = document.querySelector("#sub");
 var m = document.querySelector("#multiply");
 var d = document.querySelector("#divide");
+var cpy = document.querySelector("#copy");
 
 const first = document.querySelector("#input1");
 const second = document.querySelector("#input2");
@@ -38,8 +39,22 @@ a.addEventListener("click", add);
 s.addEventListener("click", sub);
 m.addEventListener("click", multiply);
 d.addEventListener("click", divide);
-clear.addEventListener('click', () => {
-      input1.value = "";
-      input2.value = "";
-      output.value = "";
-})
+clear.addEventListener("click", () => {
+  input1.value = "";
+  input2.value = "";
+  output.value = "";
+});
+
+cpy.addEventListener("click", () => {
+  /* Get the text field */
+  var copyText = document.getElementById("output");
+
+  /* Select the text field */
+  copyText.select();
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+});
